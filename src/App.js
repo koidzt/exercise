8a2ef9b081062f692d851ae661dae5d54be94cb5
koidzt <koi27792@gmail.com>
2021-03-11 16:18:30 +0700

    refactor code

--- src/App.js
+++ src/App.js
@@ -4,54 +4,61 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Select from 'react-select';
 
+const fetchData = async (ind) => {
+  const res = await axios.get('https://60474f19b801a40017ccbe13.mockapi.io/chart');
+  const allData = res.data;
+  const oddData = allData.filter((item) => new Date(item.createdAt).getDate() % 2 === 1);
+  const evenData = allData.filter((item) => new Date(item.createdAt).getDate() % 2 !== 1);
+
+  const dataList = [allData, oddData, evenData];
+  return dataList[ind];
+};
+
+const CreateDataBar = (data) => {
+  const dataBar = {
+    labels: data.map((item) => item.group),
+    datasets: [
+      {
+        label: 'Bar1 Chart',
+        data: data.map((item) => item.price),
+        backgroundColor: 'rgba(255,0,0,0.2)',
+        borderColor: 'rgba(255,0,0,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,0,0,0.4)',
+        hoverBorderColor: 'rgba(255,0,0,1)',
+      },
+    ],
+  };
+  return dataBar;
+};
+
 function App() {
-  const [data, setData] = useState({});
   const [dataBar, setDataBar] = useState({});
+  const [selectValue, setSelectValue] = useState(0);
+
+  const selectOption = [
+    { value: 0, label: 'All Date' },
+    { value: 1, label: 'Odd Date' },
+    { value: 2, label: 'Even Date' },
+  ];
+
+  const fetchDataBar = async () => {
+    try {
+      const data = await fetchData(selectValue);
+      const barData = CreateDataBar(data);
+      setDataBar(barData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
-    axios.get('https://60474f19b801a40017ccbe13.mockapi.io/chart').then((res) => {
-      const allData = res.data;
-      const oddData = allData.filter((item) => new Date(item.createdAt).getDate() % 2 === 1);
-      const evenData = allData.filter((item) => new Date(item.createdAt).getDate() % 2 !== 1);
-      console.log('allData =>', allData, 'oddDate =>', oddData, 'evenDate =>', evenData);
-
-      const filterData = (data) => {
-        const value = {
-          labels: data.map((item) => item.group),
-          datasets: [
-            {
-              label: 'Bar1 Chart',
-              data: data.map((item) => item.price),
-              backgroundColor: 'rgba(255,0,0,0.2)',
-              borderColor: 'rgba(255,0,0,1)',
-              borderWidth: 1,
-              hoverBackgroundColor: 'rgba(255,0,0,0.4)',
-              hoverBorderColor: 'rgba(255,0,0,1)',
-            },
-          ],
-        };
-        return value;
-      };
-
-      const select = [
-        { value: filterData(allData), label: 'All Date' },
-        { value: filterData(oddData), label: 'Odd Date' },
-        { value: filterData(evenData), label: 'Even Date' },
-      ];
-
-      console.log(select);
-      setData(select);
-    });
-  }, []);
-
-  const handleChangeSelect = (select) => {
-    console.log(select);
-    setDataBar(select.value);
-  };
+    fetchDataBar();
+  }, [selectValue]);
 
   return (
     <div className="container">
-      <Select onChange={handleChangeSelect} options={data} />
+      <Select defaultValue={selectOption[0]} onChange={(e) => setSelectValue(e.value)} options={selectOption} />
       <Bar data={dataBar} />
     </div>
   );
