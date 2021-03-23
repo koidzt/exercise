b5606e0b9453e7f01406ea9758d55f631f562ed6
koidzt <koi27792@gmail.com>
2021-03-23 16:17:52 +0700

    Clear Code to Start React

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import AssetProductCard from './components/AssetProductCard';
 
 function App() {
   return (
     <div className="App">
-      <header
-        className="App-header"
-        style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap' }}
-      >
-        {/* <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a> */}
-        <div className="my-2">
-          <AssetProductCard />
-        </div>
-        <div className="my-2">
-          <AssetProductCard />
-        </div>
-        <div className="my-2">
-          <AssetProductCard />
-        </div>
-        <div className="my-2">
-          <AssetProductCard />
-        </div>
-        <div className="my-2">
-          <AssetProductCard />
-        </div>
-        <div className="my-2">
-          <AssetProductCard />
-        </div>
-        <div className="my-2">
-          <AssetProductCard />
-        </div>
-        <div className="my-2">
-          <AssetProductCard />
-        </div>
+        </a>
       </header>
     </div>
   );
